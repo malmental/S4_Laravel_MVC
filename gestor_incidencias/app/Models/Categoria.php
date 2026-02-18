57ecdd68805b7c1001d512cf65ce074d4996c054
malmental <orlandorojasoyarzun@gmail.com>
2026-02-18 12:05:15 +0100

    Relationship between models has been configured

--- gestor_incidencias/app/Models/Categoria.php
+++ gestor_incidencias/app/Models/Categoria.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    //
+    protected $fillable = [
+        'nombre'
+    ];
+
+    public function incidencias()
+    {
+        return $this->belongsToMany(Incidencia::class);
+    }
 }
--- gestor_incidencias/app/Models/Incidencia.php
+++ gestor_incidencias/app/Models/Incidencia.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Incidencia extends Model
 {
-    //
+    protected $fillable = [
+        'titulo',
+        'descripcion',
+        'estado',
+        'prioridad',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categorias()
+    {
+        return $this->belongsToMany(Categoria::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
+
