9f6399626d650f51371533f6cfbeb798d162a679
malmental <orlandorojasoyarzun@gmail.com>
2026-03-12 23:30:52 +0100

    destroy user redirect to welcome now is working

--- gestor_incidencias/resources/views/profile/partials/delete-user-form.blade.php
+++ gestor_incidencias/resources/views/profile/partials/delete-user-form.blade.php
@@ -32,13 +32,13 @@
 
                 <x-text-input
                     id="password"
-                    name="password"
+                    name="current_password"
                     type="password"
                     class="mt-1 block w-3/4"
                     placeholder="{{ __('Password') }}"
                 />
 
-                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+                <x-input-error :messages="$errors->userDeletion->get('current_password')" class="mt-2" />
             </div>
 
             <div class="mt-6 flex justify-end">
